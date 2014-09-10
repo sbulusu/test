888282e6317d8678055dce2f2d6f74f3c3de0851
sridhar bulusu <sbulusu@paypal.com>
2014-09-10 20:02:10 +0530

    updated

---
+++ src/test/java/com/sridhar/test/firstproject/testclass1.java
@@ -0,0 +1,12 @@
+package com.sridhar.test.firstproject;
+import org.testng.annotations.Test;
+
+
+public class testclass1 {
+	
+	@Test
+	public void testmethod1(){
+		System.out.println("test1");
+	}
+
+}
